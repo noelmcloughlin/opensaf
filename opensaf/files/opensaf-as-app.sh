ef2a4784f2ff88633d6086540d4b1cbb732ef573
Noel McLoughlin <nmcloughlin@overstock.com>
2017-07-22 00:15:54 +0100

    Fedora support

---
+++ opensaf/files/opensaf-as-app.sh
@@ -0,0 +1,5 @@
+
+export PATH={{ opensaf_prefix }}/bin:${PATH}
+export PYTHONPATH={{ opensaf_prefix }}/lib/python2.7/site-packages:${PYTHONPATH}
+export LD_LIBRARY_PATH={{ opensaf_prefix }}/lib:${LD_LIBRARY_PATH}}
+
